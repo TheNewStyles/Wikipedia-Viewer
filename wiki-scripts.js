c5f5ab913e0a3e1b38989bf5e7b3547c5de99fbf
Ty <tyler.peterson@utah.edu>
2016-09-15 18:34:04 -0600

    Add jquery call to get wikimedia api

--- wiki-scripts.js
+++ wiki-scripts.js
@@ -1,3 +1,29 @@
+$(function() {
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=";
+    var thisUrl = "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?";
+
+    $.ajax({
+        type: "GET",
+        url: thisUrl,
+        contentType: "application/json; charset=utf-8",
+        async: false,
+        dataType: "json",
+        success: function (data, textStatus, jqXHR) {
+            console.log(data);
+
+            // TODO 
+            // Do bunch of stuff here
+        },
+        error: function (errorMessage) {
+        }
+    });
+
+
+
+});
+
+
+// Toggle search bar
 function searchToggle(obj, evt){
     var container = $(obj).closest('.search-wrapper');
         if(!container.hasClass('active')){
@@ -11,6 +37,7 @@ function searchToggle(obj, evt){
         }
 }
 
+// Event listeners
 var el = document.getElementById("search-icon");
 var el1 = document.getElementById("close");
 
